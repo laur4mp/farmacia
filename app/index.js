c53ae818889d359dba98d3c88a1fa90b352f7e77
vps16-lab <vps16@aluno.ifnmg.edu.br>
2025-12-05 13:25:25 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View } from "react-native";
 import Login from "../componentes/button";
-import BolasVerdes from "../componentes/fundo";
+import BolasVerdes from "../componentes/bolas_verdes";
 
 export default function LoginScreen() {
   return (
