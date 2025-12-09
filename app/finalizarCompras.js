2dfb58f81955bc56113c4658ed68938731f92f9f
Laura Maria <lm5849353@gmail.com>
2025-12-08 21:03:45 -0300

    finalizar compras

---
+++ app/finalizarCompras.js
@@ -0,0 +1,8 @@
+import React from 'react'; 
+import FinalizarCompras from "../componentes/tela_finalizarCompras";
+
+export default function FinalizarComprasScreen() {   
+    return (
+        <FinalizarCompras />
+    );
+}
